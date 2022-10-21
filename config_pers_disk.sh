549ef86822fc5796565ed458ae3e9c1a2e5c68cc
Carlos Dobler <dobler.carlos@gmail.com>
2022-10-21 16:01:56 -0400

    Update config_pers_disk.sh

--- config_pers_disk.sh
+++ config_pers_disk.sh
@@ -4,16 +4,16 @@
 sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
 
 # Create mounting point
-mkdir pers_disk
+mkdir -p /mnt/pers_disk
 
 # Mount disk
-sudo mount -o discard,defaults /dev/sdb $HOME/pers_disk
+sudo mount -o discard,defaults /mnt/pers_disk
 
 # Grant write permissions
-sudo chmod a+w $HOME/pers_disk
+sudo chmod a+w /mnt/pers_disk
 
 # Backup fstab file
 sudo cp /etc/fstab /etc/fstab.backup
 
 # Add line to fstab file
-echo "UUID=$(blkid -s UUID -o value /dev/sdb) $HOME/pers_disk ext4 discard,defaults,nofail 0 2" | sudo tee -a /etc/fstab
+echo "UUID=$(blkid -s UUID -o value /dev/sdb) /mnt/pers_disk ext4 discard,defaults,nofail 0 2" | sudo tee -a /etc/fstab
