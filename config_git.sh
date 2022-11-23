bf520ebff686cac1c3c628850bde8e8c7bad56b0
Carlos Dobler <dobler.carlos@gmail.com>
2022-11-23 15:11:39 -0500

    Update config_git.sh

--- config_git.sh
+++ config_git.sh
@@ -3,6 +3,7 @@
 git config --global user.name $1
 git config --global user.email 'dobler.carlos@gmail.com'
 
-git config --global credential.helper 'cache --timeout=10000000'
+# git config --global credential.helper 'cache --timeout=10000000'
+git config --global credential.helper store
 
 git config --global --list
