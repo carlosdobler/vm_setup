c2bdce3545fa45e4a004b73aca43ef50fd168f4d
Carlos Dobler <dobler.carlos@gmail.com>
2022-11-23 15:35:32 -0500

    Update config_buckets.sh

--- config_buckets.sh
+++ config_buckets.sh
@@ -5,3 +5,6 @@ sudo chmod a+rw /mnt/bucket_cmip5
 
 sudo mkdir /mnt/bucket_mine
 sudo chmod a+rw /mnt/bucket_mine
+
+printf '\n# Alias to mount buckets\nalias mountbuckets = "gcsfuse cmip5_data /mnt/bucket_cmip5;gcsfuse clim_data_reg_useast1 /mnt/bucket_mine"' >> ~/.bashrc
+source ~/.bashrc
