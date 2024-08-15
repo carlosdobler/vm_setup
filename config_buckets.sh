01f10cccbfcd52f6e5a43eee72506c2df3c4a45a
Carlos Dobler <dobler.carlos@gmail.com>
2024-08-15 08:39:17 -0400

    added implicit-dirs flags

--- config_buckets.sh
+++ config_buckets.sh
@@ -6,5 +6,5 @@ sudo chmod a+rw /mnt/bucket_cmip5
 sudo mkdir /mnt/bucket_mine
 sudo chmod a+rw /mnt/bucket_mine
 
-printf '\n# Alias to mount buckets\nalias mountbuckets="gcsfuse cmip5_data /mnt/bucket_cmip5;gcsfuse clim_data_reg_useast1 /mnt/bucket_mine"' >> ~/.bashrc
+printf '\n# Alias to mount buckets\nalias mountbuckets="gcsfuse cmip5_data --implicit-dirs /mnt/bucket_cmip5;gcsfuse --implicit-dirs clim_data_reg_useast1 /mnt/bucket_mine"' >> ~/.bashrc
 source ~/.bashrc
